ee49026f97fa021d5c660f90c78c32b459719ded
pume_maraj <56228758+pellumbmaraj@users.noreply.github.com>
2025-03-24 22:05:16 +0300

    Update socket.js

--- socket.js
+++ socket.js
@@ -6,8 +6,7 @@ const rooms = new Map();
 const setupSocket = (server) => {
     const io = new Server(server, {
         cors: {
-            origin: "http://localhost:5173",
-            methods: ["GET", "POST"],
+            origin: "*",
             credentials: true,
         },
     });
