0fdd7d2a3358caaa3dd349a0c4ea9980f4403b2e
pume_maraj <56228758+pellumbmaraj@users.noreply.github.com>
2025-03-24 22:04:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ let sessions = {};
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true
 }));
 app.use(cookieParser());
